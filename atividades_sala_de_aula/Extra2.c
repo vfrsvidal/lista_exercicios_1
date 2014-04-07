430de88a5e95476b2fe00ad0a7e1d4c9e78c17cf
vfrsvidal <vfrsvidal@outlook.com>
2014-04-07 18:33:20 -0300

    07_04_2014

---
+++ atividades_sala_de_aula/Extra2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//#include <bool.h>
+
+int main(){
+
+    int a = 10, b = 1, c = 12;
+
+    if (a > b + c){
+        printf("Verdadeiro!\n");
+    } else {
+        printf("Falso!\n");
+    }
+
+    return 0;
+}
